59257db8a5c6af2fe005fb49a53a7396931abc54
Evangeline Nana-Dabankah <evangelinend@gmail.com>
2020-06-14 00:00:40 +0100

    test2
    index page now exhibits single result

--- app.py
+++ app.py
@@ -9,34 +9,19 @@ app = Flask(__name__)
 
 @app.route('/result', methods=['POST'])
 def result():
-    information_mapper = {
-        "new deaths": "NewDeaths",
-        "total confirmed": "TotalConfirmed",
-        "total recovered": "TotalRecovered"
-    }
     location = request.form['local'].lower()
-    question = request.form['question'].lower()
     r = requests.get('https://api.covid19api.com/summary')
     covid_19 = r.json()
     countries = covid_19.get('Countries')
-    country_info = None
+    number_of_cases = 0
+    total_recovered = 0
     for country in countries:
         if country.get('Country').lower() == location:
-            info_key = information_mapper.get(question)
-            country_info = country.get(info_key)
+            number_of_cases = country.get('NewConfirmed')
+            total_recovered = country.get('TotalRecovered')
             break
-    #0 Look at Covid1.py file
-    #1 convert response to json
-    #2 get countries list from json
-    #3 create empty variable for storing country information
-    #4 Loop through each country in country list - For loop
-    #5 Compare country to country selection (location variable) - If statement
-    #6 If 5 is true, set variable created in 3 to country
-    #7 Break loop
-    #8 come back to me
 
-    return '{question}: {answer}'.format(question=question.title(), answer=country_info)
-    #return render_template('result.html')
+    return render_template('result.html', cases=number_of_cases, recovered=total_recovered)
 
 
 @app.route('/')
