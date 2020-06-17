e4e98e4158aa38d9cbf2efcb3d73d9033e7c9b4e
hayfaawshan <59798977+hayfaawshan@users.noreply.github.com>
2020-06-17 17:49:22 +0100

    Updating the whole app CSS files as well as HTML files

--- app.py
+++ app.py
@@ -2,17 +2,29 @@
 
 from flask import Flask, render_template, url_for, request
 import requests
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/result', methods=['POST'])
 def result():
-    location = request.form['local']
-    r = requests.get('https://api.covid19api.com/total/country/' +location )
-    json_object = r.text
-    return json_object
-    #return render_template('result.html')
+    location = request.form['local'].lower()
+    r = requests.get('https://api.covid19api.com/summary')
+    covid_19 = r.json()
+    countries = covid_19.get('Countries')
+    number_of_cases = 0
+    total_deaths = 0
+    total_recovered = 0
+
+    for country in countries:
+        if country.get('Country').lower() == location:
+            number_of_cases = country.get('TotalConfirmed')
+            total_deaths = country.get('TotalDeaths')
+            total_recovered = country.get('TotalRecovered')
+            break
+
+    return render_template('result.html', cases=number_of_cases, deaths=total_deaths, recovered=total_recovered, location=location)
 
 
 @app.route('/')
